9667adb9c03eacfaab01e2c29619bcd31c126a0f
Song Jae Hyeon <0nocsace@gmail.com>
2021-01-30 11:52:33 +0900

    AccessModifier

---
+++ GitTest1/src/ch06/AccessModifier.java
@@ -0,0 +1,24 @@
+
+package ch06;
+import java.util.*;
+
+import yj.*;
+public class AccessModifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student std = new Student();
+		std.setName("�۱浿");
+		
+		System.out.println(std.getName());
+		
+		
+          }
+	GraduatedStudent gstd = new GraduatedStudent();
+	gstd.setPhone("01012341234");
+	System.out.println(gstd.getPhone);
+		
+	}
+
+
