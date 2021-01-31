e4604339899027084337adc826ce878d4947e871
Song Jae Hyeon <0nocsace@gmail.com>
2021-01-31 12:01:14 +0900

    01/31 point 와 circle 의 실행 메서드

---
+++ GitTest1/src/ch06/Run.java
@@ -0,0 +1,42 @@
+package ch06;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Box box1 = new Box();
+//		Box box2 = new Box(20,20,30,12000);
+//
+//		System.out.println(box1);
+//		System.out.println(box2);
+//		
+//		
+//		Date date1 = new Date();
+//		Date date2 = new Date(2021,1,5);
+//		Date date3 = new Date(2021);
+//		
+//		System.out.println(date1);
+//		System.out.println(date2);
+//		System.out.println(date3);
+//		
+		
+//		Time time1 = new Time(); //00:00:00
+//		Time time2 = new Time(13, 27, 6); //13:27:6
+//		Time time3 = new Time(99,66,77); //00:00:00
+//		Time time4 = new Time(15,66,30); //15:00:30
+//	
+//		System.out.println(time1);
+//		System.out.println(time2);
+//		System.out.println(time3);
+//		System.out.println(time4);
+		
+		Point point = new Point(25,78);
+		Circle circle1 = new Circle(point,10);
+		
+		System.out.println(circle1);
+		
+		
+	
+	}
+
+}
