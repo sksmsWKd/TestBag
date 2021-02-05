1c93cf1f9d2307a66152025fc749f99b21ee07d9
Song Jae Hyeon <0nocsace@gmail.com>
2021-02-05 12:16:28 +0900

    동물 객체 실행 메서드

---
+++ GitTest1/src/ch07/AnimalTest.java
@@ -0,0 +1,20 @@
+package ch07;
+
+public class AnimalTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Lion lion = new Lion();
+		lion.eat();
+		lion.sleep();
+		lion.roar();
+		
+		
+		Eagle eagle = new Eagle();
+		eagle.eat();
+		eagle.sleep();
+		eagle.fly();
+		
+	}
+
+}
