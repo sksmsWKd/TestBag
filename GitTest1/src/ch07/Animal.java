4f75781417b4a54ac7b8bb26787b8a907e8b17bb
Song Jae Hyeon <0nocsace@gmail.com>
2021-02-05 12:15:30 +0900

    동물 객체 실행 메서드

---
+++ GitTest1/src/ch07/Animal.java
@@ -0,0 +1,14 @@
+package ch07;
+
+public class Animal {
+private double weight;
+private String picture;
+
+void eat() {
+	System.out.println("eat()가 호출되었음");
+	
+}
+void sleep() { 
+	System.out.println("slee-p호출");
+}
+}
