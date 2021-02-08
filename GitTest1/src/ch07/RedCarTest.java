1398b2ad96c4756fda07d3bf771ba827d516a068
Song Jae Hyeon <0nocsace@gmail.com>
2021-02-08 20:21:58 +0900

    02/08 redcar 객체에 속성추가

---
+++ GitTest1/src/ch07/RedCarTest.java
@@ -0,0 +1,14 @@
+package ch07;
+
+public class RedCarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		RedIornCar rc2 = new RedIornCar();
+		rc2.speed = 100;
+		rc2.setSpeed =110;
+		
+	}
+
+}
