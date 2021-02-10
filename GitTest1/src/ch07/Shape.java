8688cfb400e6e445a8fc734d7400cd3e52bf7dcb
Song Jae Hyeon <0nocsace@gmail.com>
2021-02-10 16:24:12 +0900

    shape 객체 생성하고 메서드들 지정

---
+++ GitTest1/src/ch07/Shape.java
@@ -0,0 +1,29 @@
+package ch07;
+
+public class Shape {
+private int x;
+private int y;
+
+
+
+
+public Shape() {
+	System.out.println("shape1");
+}
+public int getX() {
+	return x;
+}
+
+public void setX(int x) {
+	this.x = x;
+}
+public int getY() {
+	return y;
+}
+public void setY(int y) {
+	this.y = y;
+}
+void print() {
+	System.out.println("XÁÂÇ¥"+ x + " yÁÂÇ¥" +  y );
+}
+}
