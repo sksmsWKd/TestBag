4492145336adf9ecb8e755ab227b7a610a43fb53
Song Jae Hyeon <0nocsace@gmail.com>
2021-02-13 17:29:40 +0900

    자바 gui 프레임 닫기 테스트

---
+++ GitTest1/src/ch08/FrameTest.java
@@ -0,0 +1,16 @@
+package ch08;
+
+import javax.swing.*;
+
+public class FrameTest {
+public static void main(String[]args) {
+	JFrame frame = new JFrame();
+	
+	frame.setSize(300,200);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+	
+	
+}
+
+}
