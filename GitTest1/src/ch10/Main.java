b0481de0866ca8b923c1461a553b21791c442404
Song Jae Hyeon <0nocsace@gmail.com>
2021-02-15 17:32:13 +0900

    GUI 버튼 누르기 ActionListener 사용

---
+++ GitTest1/src/ch10/Main.java
@@ -0,0 +1,12 @@
+package ch10;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyFrame1 frame = new MyFrame1();
+		//JFrame frame = new MyFrame1();
+		
+	}
+
+}
---
+++ GitTest1/src/ch10/MyFrame1.java
@@ -0,0 +1,27 @@
+package ch10;
+import javax.swing.*;
+public class MyFrame1 extends JFrame{
+
+	
+	private JButton button;
+	private JLabel label;
+	
+	public MyFrame1() {
+		this.setSize(300,200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("이벤트예제");
+		
+		JPanel panel= new JPanel();
+		
+		button = new JButton("버튼을 누르세요");
+		button.addActionListener(new MyListener1());
+		label = new JLabel("아직 버튼이 눌려지지 않았습ㄴ디ㅏ");
+		
+		panel.add(button);
+		panel.add(label);
+		
+		this.add(panel);
+		this.setVisible(true);
+		
+	}
+}
