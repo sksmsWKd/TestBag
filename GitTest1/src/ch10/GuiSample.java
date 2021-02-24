2259cd6805fcac8265e5a384775af73d87c1f13b
Song Jae Hyeon <0nocsace@gmail.com>
2021-02-24 14:14:01 +0900

    1

---
+++ GitTest1/src/ch10/GuiSample.java
@@ -0,0 +1,36 @@
+package ch10;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GuiSample extends JFrame{
+
+	
+	private JPanel panel; 
+	private JButton btn1; 
+	private JLabel label;
+	
+	
+	public GuiSample(){
+	
+		JFrame frame  = new JFrame();
+		
+		this.add(frame);
+		
+		this.setVisible(true);
+		this.setSize(500,500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.add(panel);
+	}
+
+	
+	
+	
+
+public static void main(String[]args) {
+	JFrame frame = new JFrame();
+	
+}
+}
+
