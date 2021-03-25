2049486fd1b2d40a0c9f0e9014c46a91d53ad5a8
Song Jae Hyeon <0nocsace@gmail.com>
2021-03-25 17:35:25 +0900

    21/03/25 OUT OF BOUND EXCEPTION 자바 실습입니다

---
+++ JavaTest1/src/OutOfResourcesException.java
@@ -0,0 +1,10 @@
+
+public class OutOfResourcesException extends Exception {
+	public OutOfResourcesException (){
+		System.out.println("OutOfResourcesException 생성 완료");
+	}
+	public OutOfResourcesException(String msg) {
+		
+		System.out.println("OutOfResourcesException 생성 완료");
+	}
+}
