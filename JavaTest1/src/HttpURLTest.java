e51cb43c6fa04bb3f32c43b06cccab46f2617edc
Song Jae Hyeon <0nocsace@gmail.com>
2021-06-03 11:22:31 +0900

    21/06/03 GET방식으로 페이지의 HTML 코드를 한줄씩 bufferedReader

---
+++ JavaTest1/src/HttpURLTest.java
@@ -0,0 +1,30 @@
+import java.net.*;
+import java.io.*;
+public class HttpURLTest {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		URL url =new URL ("https://www.google.com/search?q=java");
+		HttpURLConnection con = (HttpURLConnection)url.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		
+		 int responseCode = con.getResponseCode();
+		 System.out.println("Response Code :  "+ responseCode);
+		 
+		 InputStream stream = con.getInputStream();
+		 
+		 InputStreamReader isreader  = new InputStreamReader(stream,"UTF-8");
+		 
+		 BufferedReader reader = new BufferedReader(isreader);
+			String line ;
+			while((line = reader.readLine())!=null) {
+			System.out.println(line);
+			
+			}
+		 // 이거아직 커밋안함
+		 
+		
+	}
+
+}
