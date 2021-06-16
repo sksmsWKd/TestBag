829dd6cd4d0861eeace578af43d1375c55faa440
Song Jae Hyeon <0nocsace@gmail.com>
2021-06-16 21:55:22 +0900

    COLLECTION 들의 자료출력

---
+++ JavaTest1/src/tt/CollectionTest3.java
@@ -0,0 +1,31 @@
+package tt;
+
+import java.util.*;
+public class CollectionTest3 {
+
+	public static void test1() {
+		List<Integer> list1 = new ArrayList<>();
+		List<String> list2 = new LinkedList<>();
+		List<Double> list3 = new Vector<>();
+		List<Integer> list4 = new Stack<>();
+		
+		
+		for(int i = 0 ; i<10 ; i++) {
+			list1.add(i+1);
+			list2.add(String.valueOf(i+1));
+			list3.add(i+1 *1.0);
+			list4.add(10-i);
+			
+		}
+		
+		System.out.println(list1);
+		System.out.println(list2);
+		System.out.println(list3);
+		System.out.println(list4);
+
+	}
+	
+	public static void main(String[] args){
+		test1();
+	}
+}
